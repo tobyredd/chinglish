ebd87e3017d913cba510e0b74b58658081819fad
Toby Redd <tobyredd@go.byuh.edu>
2015-01-17 14:47:06 -0800

    chinese characters are now falling from the sky. now working on the user portion of the game.

--- public/javascripts/controllers/games.js
+++ public/javascripts/controllers/games.js
@@ -1,16 +1,18 @@
 chinglish.controller('games', function($scope, gameFactory){
-	$scope.words = gameFactory.Word(function(data){$scope.words = data})
+	$scope.words = gameFactory.getWords(function(data){$scope.words = data})
 	$scope.submit = function() {
 		console.log($scope.text);
 		gameFactory.submitAnswer($scope.text);
 	}
+	function timerTick(myFactory){
+		myFactory.playGame();
+	}
 	$scope.start = function(screen, user_input, score){
 		console.log('stuff', screen, user_input, score);
 		var screen = document.getElementById(screen);
-		// console.log('hello start', screen);
-		// gameFactory.startGame(screen, user_input, score);
-		
-		setInterval(gameFactory.playGame(screen, user_input, score), 500);
+		gameFactory.playGround(screen, user_input, score);
+		setInterval(timerTick, 500, gameFactory);
+		// timerTick(gameFactory);
 	}
 
   
--- public/javascripts/services/game_factory.js
+++ public/javascripts/services/game_factory.js
@@ -1,73 +1,88 @@
 chinglish.factory('gameFactory', ['$http', function($http){
 	var words = [];
+	this.user_score = 0;
+	this.counter = 0;
 	var factory = {};
-	factory.Word = function(callback){
+	factory.getWords = function(callback){
 		$http.get('/words/index_json').success(function(output){
 			words = output;
 			callback(output);
-			console.log('words: ', words[1].chinese);
+			// console.log('words: ', words);
 
 		})
 		return words;
 	}
 	factory.submitAnswer = function(info){
-		console.log('info: ', info);
+		// console.log('info: ', info);
+		ini_score = this.score;
+		for(i in this.words){
+			if(this.words[i].english === info){
+				console.log('yes!!!');
+				$('#word_' + this.words[i].id).remove();
+				this.score = this.score + 5;
+			}
+		}
 	}
 
-	factory.playGame = function(screen, user_input, score){
-		console.log('passed in: ', screen, user_input, score);
-		this.screen = screen
-		this.user_input = user_input;
-		this.words = [];
-		this.user_score = 0;
-		this.score = score;
-		this.counter = 0;
-
-		this.createWord = function(){
+	factory.createWord = function(){
 			this.counter++;
 			var newWord = new Word(this.counter);
 
 			newWord.createRandomWordsBetween(350, 1000);
 			this.words.push(newWord);
-			console.log('game words: ', words);
-			console.log('newWord id: ', newWord.id);
+			// console.log('game words: ', words);
+			// console.log('newWord id: ', newWord.id);
 			my_string = "<div id='word_" + (newWord.id) + "'><div style='position: absolute; left:" + newWord.x + "px; top:" + newWord.y + "px'>" + newWord.word + "</div></div>";
-			console.log(my_string);
+			// console.log(my_string);
 			this.screen.innerHTML = this.screen.innerHTML + my_string;
-		}
-		this.fallWords = function(){
-			console.log('made it to the fall words functino!');
-			for(i = this.words.length - 1; i >= 0; i--){
-				this.words[i].y = this.words[i].y + 15;
-				if(this.words[i].y > 385){
-					$('#word_' + this.words[i].id).remove();
-					if(this.words[i].word != ''){
-						this.score = this.score - 1;
-					}
-					$('#' + this.user_score).text(this.score);
-					this.words.shift();
-					
+	}
+	factory.fallWords = function(){
+		// console.log('made it to the fall words functino!');
+		for(i = this.words.length - 1; i >= 0; i--){
+			this.words[i].y = this.words[i].y + 15;
+			if(this.words[i].y > 385){
+				$('#word_' + this.words[i].id).remove();
+				if(this.words[i].word != ''){
+					this.score = this.score - 1;
 				}
+				$('#' + this.user_score).text(this.score);
+				this.words.shift();
+				
 			}
 		}
-		this.updatePosition = function(){
-			for(i in this.words){
-				loc = document.getElementById('word_'+this.words[i].id);
-				if(loc){
-					loc.innerHTML = "<div style='position: absolute; left:" + this.words[i].x + "px; top:" + this.words[i].y + "px'>" + this.words[i].word + "</div>";
+	}
+	factory.updatePosition = function(){
+		for(i in this.words){
+			loc = document.getElementById('word_'+this.words[i].id);
+			if(loc){
+				loc.innerHTML = "<div style='position: absolute; left:" + this.words[i].x + "px; top:" + this.words[i].y + "px'>" + this.words[i].word + "</div>";
 
-				}
 			}
 		}
+	}
+	factory.playGround = function(screen, user_input, score){
+		this.screen = screen
+		this.user_input = user_input;
+		this.score = score;
+		this.words = words;
+		this.score = 0;
+		this.counter = 0;
+	}
+	factory.playGame = function(){
+		// console.log('passed in: ', screen, user_input, score);
+		
+		
 		this.createWord();
 		this.fallWords();
 		this.updatePosition();
 	}
+
 	function Word(id){
+	
 		this.x = 0;
 		this.y = 0;
 		this.id = id;
-		console.log('id;', id);
+		// console.log('id;', id);	
 
 		this.createRandomWordsBetween = function(x_min, x_max)
 		{
@@ -76,12 +91,14 @@ chinglish.factory('gameFactory', ['$http', function($http){
 			random_x_index = parseInt(Math.random()*(750));
 			this.x = random_x_index;
 		}
-	}	
+	}
 
 // ////////this will need to be pulling from the db, where we will save {chinese: '九', english: 'nine'},,,,, or possibly{chinese: IMG, english: translate}///
 
 // 	///////end here/////
 	return factory;
-}])
+}]);
+
+
 
 
